d6f7b3420fbfcbfc7e9c5f6bff4ec7a83bbd9606
Syadon <bokagabor4@gmail.com>
2022-08-31 20:46:02 +0200

    mongoose create, delete

--- app.js
+++ app.js
@@ -1,42 +1,68 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const date = require(__dirname + "/date.js");
+const mongoose = require('mongoose');
 const app = express();
 
-const items = [];
-const workItems = [];
+
+mongoose.connect("mongodb://localhost:27017/shoppinglistDB");
+const itemsSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    }
+});
+
+const Item = mongoose.model('Item', itemsSchema);
+
+// const itemOne = new Item({
+//     name: "Kakaó"
+// });
+
+// const itemTwo = new Item({
+//     name: "Zsebkendő"
+// });
+
+// const itemThree = new Item({
+//     name: "Kukorica"
+// });
+
+// const defaultItems = [itemOne, itemTwo, itemThree];
+
+// Item.insertMany(defaultItems, (err) => {
+//     if(err) return console.log(err);
+//     console.log("Default items inserted.");
+// });
 
 app.set('view engine', 'ejs');
-
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 app.get('/', (req, res) => {
     const day = date.getDate();
-    res.render('list', {listTitle: day, newListItems: items});
+    Item.find({}, (err, results) => {
+        if(err) return console.log(err);
+        res.render('list', {listTitle: `${day}  Bevásárlólista`, newListItems: results});
+    });
 });
 
 app.post('/', (req, res) => {
-    const item = req.body.newItem;
-    if(req.body.list === "Work") {
-        workItems.push(item);
-        res.redirect('/work');
-    }
-    else {
-        items.push(item);
-        res.redirect("/");
-    }
+    const item = new Item({
+        name: req.body.newItem
+    });
+    item.save();
+    res.redirect("/");
 });
 
-app.get('/work', (req, res) => {
-    res.render('list', {listTitle: 'Work List', newListItems: workItems});
-})
+app.post('/delete', (req, res) => {
+    const checkedItemId = req.body.checkbox;
+    Item.findByIdAndRemove(checkedItemId, (err) => {
+        if(err) return console.log(err);
+        res.redirect('/');
+    });
+});
 
-app.get('/about', (req, res) => {
-    res.render('about');
-})
-
-const server = app.listen(process.env.PORT || 8500, () => {
+const server = app.listen(process.env.PORT || 10000, () => {
     console.log(`Server started on port ${server.address().port}`);
 })
