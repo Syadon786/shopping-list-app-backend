1b731582ce2b086094b8692bac0944526ea103ca
Syadon <bokagabor4@gmail.com>
2022-09-01 11:29:24 +0200

    custom lists with route parameters

--- app.js
+++ app.js
@@ -3,12 +3,17 @@ const bodyParser = require('body-parser');
 const date = require(__dirname + "/date.js");
 const mongoose = require('mongoose');
 require('dotenv').config();
+const _ = require('lodash');
 const app = express();
 
 
 mongoose.connect(process.env.DATABASE_URL);
 const itemsSchema = mongoose.Schema({
     name: {
+        type: String,
+        required: true
+    },
+    listId: {
         type: String,
         required: true
     }
@@ -40,17 +45,19 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+
 app.get('/', (req, res) => {
     const day = date.getDate();
-    Item.find({}, (err, results) => {
+    Item.find({listId: "test"}, (err, results) => {
         if(err) return console.log(err);
-        res.render('list', {listTitle: `${day}  Bevásárlólista`, newListItems: results});
+        res.render('list', {listTitle: `${day}  Bevásárlólista`, newListItems: results, listid: undefined});
     });
 });
 
 app.post('/', (req, res) => {
     const item = new Item({
-        name: req.body.newItem
+        name: req.body.newItem,
+        listId: "test"
     });
     item.save();
     res.redirect("/");
@@ -58,11 +65,42 @@ app.post('/', (req, res) => {
 
 app.post('/delete', (req, res) => {
     const checkedItemId = req.body.checkbox;
-    Item.findByIdAndRemove(checkedItemId, (err) => {
+    Item.deleteOne({listId: "test", _id: checkedItemId}, (err) => {
         if(err) return console.log(err);
-        res.redirect('/');
+        res.redirect('/'); 
     });
 });
+
+app.get('/list/:listid', (req, res) => {
+    const day = date.getDate();
+    const listId = _.lowerCase(req.params.listid);
+    Item.find({listId: listId}, (err, results) => {
+        if(err) return console.log(err);
+        res.render('list', {listTitle: `${day}  Bevásárlólista`, newListItems: results, listid: listId});
+    });
+});
+
+
+app.post('/list/:listid', (req, res) => {
+    const listId = _.lowerCase(req.params.listid);
+    const item = new Item({
+        name: req.body.newItem,
+        listId: listId
+    });
+    item.save();
+    res.redirect(`/list/${listId}`);
+});
+
+
+app.post('/delete/:listid', (req, res) => {
+    const checkedItemId = req.body.checkbox;
+    const listId = _.lowerCase(req.params.listid);
+    Item.deleteOne({listId: listId, _id: checkedItemId}, (err) => {
+        if(err) return console.log(err);
+        res.redirect(`/list/${listId}`);
+    });
+});
+
 
 const server = app.listen(process.env.PORT || 10000, () => {
     console.log(`Server started on port ${server.address().port}`);
