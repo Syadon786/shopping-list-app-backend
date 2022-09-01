025c2abaa221f4236ae9321bf57df59383aaea5a
Syadon <bokagabor4@gmail.com>
2022-09-01 16:00:56 +0200

    added shoppinglist creating restriction and updated database model

--- app.js
+++ app.js
@@ -12,33 +12,19 @@ const itemsSchema = mongoose.Schema({
     name: {
         type: String,
         required: true
-    },
-    listId: {
-        type: String,
-        required: true
     }
 });
 
+const listSchema = mongoose.Schema({
+    listid: {
+        type: String,
+        required: true
+    },
+    items: [itemsSchema]
+});
+
 const Item = mongoose.model('Item', itemsSchema);
-
-// const itemOne = new Item({
-//     name: "Kakaó"
-// });
-
-// const itemTwo = new Item({
-//     name: "Zsebkendő"
-// });
-
-// const itemThree = new Item({
-//     name: "Kukorica"
-// });
-
-// const defaultItems = [itemOne, itemTwo, itemThree];
-
-// Item.insertMany(defaultItems, (err) => {
-//     if(err) return console.log(err);
-//     console.log("Default items inserted.");
-// });
+const List = mongoose.model('List', listSchema);
 
 app.set('view engine', 'ejs');
 
@@ -48,54 +34,62 @@ app.use(express.static("public"));
 
 app.get('/', (req, res) => {
     const day = date.getDate();
-    Item.find({listId: "test"}, (err, results) => {
+    List.findOne({listid : "test"}, (err, foundList) => { 
         if(err) return console.log(err);
-        res.render('list', {listTitle: `${day}  Bevásárlólista`, newListItems: results, listid: undefined});
+        res.render('list', {listTitle: `${day}  Bevásárlólista`, newListItems: foundList.items, listid: undefined});
     });
 });
 
 app.post('/', (req, res) => {
-    const item = new Item({
-        name: req.body.newItem,
-        listId: "test"
+    List.findOne({listid : "test"}, (err, foundList) => { 
+        if(err) return console.log(err);
+        const item = new Item({
+            name: req.body.newItem,
+        });
+        foundList.items.push(item);
+        foundList.save();
+        res.redirect("/");
     });
-    item.save();
-    res.redirect("/");
 });
 
 app.post('/delete', (req, res) => {
     const checkedItemId = req.body.checkbox;
-    Item.deleteOne({listId: "test", _id: checkedItemId}, (err) => {
+    List.findOneAndUpdate({listid : "test"}, {$pull: {items: {_id: checkedItemId}}}, (err) => { 
         if(err) return console.log(err);
-        res.redirect('/'); 
+        res.redirect("/");
     });
 });
 
 app.get('/list/:listid', (req, res) => {
     const day = date.getDate();
     const listId = _.lowerCase(req.params.listid);
-    Item.find({listId: listId}, (err, results) => {
+    List.findOne({listid : listId}, (err, foundList) => {
         if(err) return console.log(err);
-        res.render('list', {listTitle: `${day}  Bevásárlólista`, newListItems: results, listid: listId});
-    });
+        if(!foundList) res.redirect('/');
+        res.render('list', {listTitle: `${day}  Bevásárlólista`, newListItems: foundList.items, listid: listId});
+    });  
 });
 
 
 app.post('/list/:listid', (req, res) => {
     const listId = _.lowerCase(req.params.listid);
-    const item = new Item({
-        name: req.body.newItem,
-        listId: listId
+    List.findOne({listid : listId}, (err, foundList) => { 
+        if(err) return console.log(err);
+        if(!foundList) res.redirect('/');
+        const item = new Item({
+            name: req.body.newItem,
+        });
+        foundList.items.push(item);
+        foundList.save();
+        res.redirect(`/list/${listId}`);
     });
-    item.save();
-    res.redirect(`/list/${listId}`);
 });
 
 
 app.post('/delete/:listid', (req, res) => {
     const checkedItemId = req.body.checkbox;
     const listId = _.lowerCase(req.params.listid);
-    Item.deleteOne({listId: listId, _id: checkedItemId}, (err) => {
+    List.findOneAndUpdate({listid : listId}, {$pull: {items: {_id: checkedItemId}}}, (err) => { 
         if(err) return console.log(err);
         res.redirect(`/list/${listId}`);
     });
